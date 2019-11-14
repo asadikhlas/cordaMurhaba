296f9b9bedb5ee22e58000624930817dde59e8fb
Hammad Ahmed <hammad.tco@gmail.com>
2019-11-14 16:38:57 +0500

    testing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import MurhabaTable from './MurhabaTable/MurhabaTable'
 import Routes from './Routes'
 
+// dadha
 class App extends Component {
   render() {
     return (
